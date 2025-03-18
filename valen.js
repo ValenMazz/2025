cfd40fc96606c5990454f63a68c51b692e402500
alumno <alumno@Instituto.local>
2025-03-18 09:50:48 -0300

    practica 13/03/25

--- valen.js
+++ valen.js
@@ -1,6 +1,27 @@
-alert("holaa, todo bien??")
+/*alert("holaa, todo bien??")
 
 let añoNacimiento=2009
 let añoActual=2025
 
-alert ( ("mi edad es") + (añoActual-añoNacimiento) )
+alert ( ("mi edad es ") + (añoActual-añoNacimiento) )*/
+
+/*
+let nombreAlumno= "valen"
+let aprobado= true
+let edad= 17
+
+if (edad >= 17){
+    alert("podes ingresar")
+
+} else{
+    alert("no podes ingresar")
+}
+*/
+
+valor = 17
+
+if (edad == 17){
+    alert ("podes ingresar")
+} else{
+    alert ("no podes ingresar")
+}
